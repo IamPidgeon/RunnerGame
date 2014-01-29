f5e932298adf00c460a036b54268667703628432
IgorMBorushek <igor.borushek@gmail.com>
2014-01-29 12:53:21 -0500

    Midway commit

---
+++ Assets/Booster/BoosterManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// assign it inside SkylineManager, but not very OOP
+// should disappear if GameOver
+// I need to know the position of the skyline platforms?
+
+
+public class BoosterManager : MonoBehaviour {
+
+	public Vector3 offset, rotationVelocity;		// offset from platform's center, velocity for spin
+	public float recycleOffset, spawnChance;		// offset from Runner/spawn position, % chance for spawn
+	public GameObject prefab;
+
+	/* void Start () {
+		GameEventManager.GameOver += GameOver;
+		gameObject.SetActive (false);
+	}
+	*/
+
+	public void SpawnIfAvailable (Vector3 position) {	// spawns if booster is not already active and > % spawnChance
+		if (spawnChance >= Random.Range(0, 100)) {
+			Instantiate(prefab, new Vector3(position.x + recycleOffset, position.y + offset.y, position.z), Quaternion.identity);
+		}
+	}
+
+	private void OnTriggerEnter() {
+		Destroy(gameObject);
+		Runner.PoweredUp = true;
+	}
+
+}
---
+++ Assets/Runner/Runner.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class Runner : MonoBehaviour {
+
+	public static float distanceTraveled;
+	
+	public float acceleration;
+	public Vector3 jumpVelocity;
+	public float gameOverY;
+
+	public static bool PoweredUp;
+
+	private bool touchingPlatform;
+	private Vector3 startPosition;
+	
+	void Start () {
+		GameEventManager.GameStart += GameStart;
+		GameEventManager.GameOver += GameOver;
+		startPosition = transform.localPosition;
+		renderer.enabled = false;
+		rigidbody.isKinematic = true;
+		enabled = false;
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown("Jump") && touchingPlatform) {
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+			touchingPlatform = false;
+		}
+		else if (Input.GetButtonDown("Jump") && PoweredUp) {
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+			PoweredUp = false;
+		}
+		distanceTraveled = transform.localPosition.x;
+		
+		if (transform.localPosition.y < gameOverY) {
+			GameEventManager.TriggerGameOver();
+		}
+	}
+
+	void FixedUpdate () {
+		if(touchingPlatform){
+			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		}
+	}
+
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+
+	void OnCollisionExit () {
+		touchingPlatform = false;
+	}
+
+	private void GameStart () {
+		distanceTraveled = 0f;
+		transform.localPosition = startPosition;
+		renderer.enabled = true;
+		rigidbody.isKinematic = false;
+		enabled = true;
+	}
+	
+	private void GameOver () {
+		renderer.enabled = false;
+		rigidbody.isKinematic = true;
+		enabled = false;
+	}
+}
